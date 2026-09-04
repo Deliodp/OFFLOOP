d0502208e29458e563787c556d0693bf092bc6c8
Deliodp <deliodp4@gmail.com>
2026-09-04 16:58:52 +0200

    Update SocialBrowserView.swift

--- SocialControl-v0.3-CI/SocialControl-v0.3-CI/App/Views/SocialBrowserView.swift
+++ SocialControl-v0.3-CI/SocialControl-v0.3-CI/App/Views/SocialBrowserView.swift
@@ -21,26 +21,35 @@ struct SocialBrowserView: View {
 }
 
 private struct PersistentSocialWebView: UIViewRepresentable {
+
     let platform: SocialPlatform
     let blockReels: Bool
     let blockShorts: Bool
     let blockAds: Bool
 
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(
+            platform: platform,
+            blockReels: blockReels,
+            blockShorts: blockShorts,
+            blockAds: blockAds
+        )
     }
 
     func makeUIView(context: Context) -> UIView {
+
         let container = UIView()
         container.backgroundColor = .systemBackground
 
         Task { @MainActor in
-            let configuration = await BrowserSession.shared.makeConfiguration(
-                platform: platform,
-                blockReels: blockReels,
-                blockShorts: blockShorts,
-                blockAds: blockAds
-            )
+
+            let configuration =
+                await BrowserSession.shared.makeConfiguration(
+                    platform: platform,
+                    blockReels: blockReels,
+                    blockShorts: blockShorts,
+                    blockAds: blockAds
+                )
 
             let webView = WKWebView(
                 frame: .zero,
@@ -48,42 +57,64 @@ private struct PersistentSocialWebView: UIViewRepresentable {
             )
 
             webView.translatesAutoresizingMaskIntoConstraints = false
+
             webView.navigationDelegate = context.coordinator
+
             webView.allowsBackForwardNavigationGestures = true
+
             webView.scrollView.keyboardDismissMode = .interactive
 
             container.addSubview(webView)
 
             NSLayoutConstraint.activate([
-                webView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-                webView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-                webView.topAnchor.constraint(equalTo: container.topAnchor),
-                webView.bottomAnchor.constraint(equalTo: container.bottomAnchor)
+                webView.leadingAnchor.constraint(
+                    equalTo: container.leadingAnchor
+                ),
+                webView.trailingAnchor.constraint(
+                    equalTo: container.trailingAnchor
+                ),
+                webView.topAnchor.constraint(
+                    equalTo: container.topAnchor
+                ),
+                webView.bottomAnchor.constraint(
+                    equalTo: container.bottomAnchor
+                )
             ])
 
             context.coordinator.webView = webView
 
-            let arguments = ProcessInfo.processInfo.arguments
+            let arguments =
+                ProcessInfo.processInfo.arguments
 
             var debugURL: URL?
 
-            if let index = arguments.firstIndex(of: "-debugURL"),
+            if let index =
+                arguments.firstIndex(of: "-debugURL"),
                arguments.indices.contains(index + 1) {
-                debugURL = URL(string: arguments[index + 1])
+
+                debugURL =
+                    URL(string: arguments[index + 1])
             }
 
             let defaultURL: URL
 
             switch platform {
+
             case .instagram:
-                defaultURL = URL(
-                    string: "https://www.instagram.com/"
-                )!
+
+                defaultURL =
+                    URL(
+                        string:
+                            "https://www.instagram.com/"
+                    )!
 
             case .youtube:
-                defaultURL = URL(
-                    string: "https://m.youtube.com/"
-                )!
+
+                defaultURL =
+                    URL(
+                        string:
+                            "https://m.youtube.com/"
+                    )!
             }
 
             webView.load(
@@ -100,16 +131,27 @@ private struct PersistentSocialWebView: UIViewRepresentable {
         _ uiView: UIView,
         context: Context
     ) {
-        guard let webView = context.coordinator.webView else {
-            return
-        }
 
-        let script = FilterScriptProvider.script(
+        context.coordinator.updateSettings(
             platform: platform,
             blockReels: blockReels,
             blockShorts: blockShorts,
             blockAds: blockAds
         )
+
+        guard let webView =
+                context.coordinator.webView
+        else {
+            return
+        }
+
+        let script =
+            FilterScriptProvider.script(
+                platform: platform,
+                blockReels: blockReels,
+                blockShorts: blockShorts,
+                blockAds: blockAds
+            )
 
         webView.evaluateJavaScript(
             script,
@@ -117,44 +159,152 @@ private struct PersistentSocialWebView: UIViewRepresentable {
         )
     }
 
-    final class Coordinator: NSObject, WKNavigationDelegate {
+    final class Coordinator:
+        NSObject,
+        WKNavigationDelegate {
 
         weak var webView: WKWebView?
+
+        private var platform: SocialPlatform
+        private var blockReels: Bool
+        private var blockShorts: Bool
+        private var blockAds: Bool
+
+        init(
+            platform: SocialPlatform,
+            blockReels: Bool,
+            blockShorts: Bool,
+            blockAds: Bool
+        ) {
+
+            self.platform = platform
+            self.blockReels = blockReels
+            self.blockShorts = blockShorts
+            self.blockAds = blockAds
+        }
+
+        func updateSettings(
+            platform: SocialPlatform,
+            blockReels: Bool,
+            blockShorts: Bool,
+            blockAds: Bool
+        ) {
+
+            self.platform = platform
+            self.blockReels = blockReels
+            self.blockShorts = blockShorts
+            self.blockAds = blockAds
+        }
+
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction:
+                WKNavigationAction,
+            decisionHandler:
+                @escaping (WKNavigationActionPolicy) -> Void
+        ) {
+
+            guard let url =
+                    navigationAction.request.url
+            else {
+
+                decisionHandler(.allow)
+                return
+            }
+
+            let host =
+                url.host?.lowercased() ?? ""
+
+            let path =
+                url.path.lowercased()
+
+            // ----------------------------------
+            // INSTAGRAM REELS
+            // ----------------------------------
+
+            if (
+                blockReels &&
+                host.contains("instagram.com") &&
+                (
+                    path.hasPrefix("/reel/") ||
+                    path.hasPrefix("/reels")
+                )
+            ) {
+
+                decisionHandler(.cancel)
+
+                let safeURL =
+                    URL(
+                        string:
+                            "https://www.instagram.com/"
+                    )!
+
+                DispatchQueue.main.async {
+
+                    webView.load(
+                        URLRequest(
+                            url: safeURL
+                        )
+                    )
+                }
+
+                return
+            }
+
+            // ----------------------------------
+            // YOUTUBE SHORTS
+            // ----------------------------------
+
+            if (
+                blockShorts &&
+                host.contains("youtube.com") &&
+                (
+                    path == "/shorts" ||
+                    path == "/shorts/" ||
+                    path.hasPrefix("/shorts/")
+                )
+            ) {
+
+                decisionHandler(.cancel)
+
+                let safeURL =
+                    URL(
+                        string:
+                            "https://m.youtube.com/"
+                    )!
+
+                DispatchQueue.main.async {
+
+                    webView.load(
+                        URLRequest(
+                            url: safeURL
+                        )
+                    )
+                }
+
+                return
+            }
+
+            decisionHandler(.allow)
+        }
 
         func webView(
             _ webView: WKWebView,
             didFinish navigation: WKNavigation!
         ) {
-            let script = FilterScriptProvider.script(
-                platform: inferPlatform(from: webView.url),
-                blockReels: true,
-                blockShorts: true,
-                blockAds: false
-            )
+
+            let script =
+                FilterScriptProvider.script(
+                    platform: platform,
+                    blockReels: blockReels,
+                    blockShorts: blockShorts,
+                    blockAds: blockAds
+                )
 
             webView.evaluateJavaScript(
                 script,
                 completionHandler: nil
             )
-        }
-
-        private func inferPlatform(
-            from url: URL?
-        ) -> SocialPlatform {
-
-            if url?.host?.contains("instagram.com") == true {
-                return .instagram
-            }
-
-            return .youtube
-        }
-
-        func webView(
-            _ webView: WKWebView,
-            decidePolicyFor navigationAction: WKNavigationAction,
-            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-        ) {
-            decisionHandler(.allow)
         }
     }
 }
